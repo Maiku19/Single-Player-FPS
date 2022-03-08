74b71afc21dd2d115bd61bfa01d2520bdada95ca
Mike041905 <91633676+Mike041905@users.noreply.github.com>
2022-03-08 01:40:51 +0100

    (0.5.0.4) Updated Custom Game Menu

--- Assets/CustomGameMenu.cs
+++ Assets/CustomGameMenu.cs
@@ -1,28 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class CustomGameMenu : MonoBehaviour
 {
     [SerializeField] private MapSelector mapSelectionMenu;
+    [Space(10)]
 
-    private int playersOnTeam1;
-    private int playersOnTeam2;
+    [SerializeField] private TextMeshProUGUI playersOnTeam1;
+    [SerializeField] private TextMeshProUGUI playersOnTeam2;
+    [Space(10)]
 
-    private bool useDynamicDifficultyTeam1 = true;
-    private bool useDynamicDifficultyTeam2 = true;
-    private float difficultyTeam1 = 10;
-    private float difficultyTeam2 = 10;
+    [SerializeField] private Toggle useDynamicDifficultyTeam1;
+    [SerializeField] private Toggle useDynamicDifficultyTeam2;
+    [Space(10)]
+
+    [SerializeField] private TextMeshProUGUI difficultyTeam1;
+    [SerializeField] private TextMeshProUGUI difficultyTeam2;
 
     public void StartGame()
     {
-        PlayerPrefs.SetInt("Players Team1", playersOnTeam1);
-        PlayerPrefs.SetInt("Players Team2", playersOnTeam2);
+        PlayerPrefs.SetInt("Players Team1", int.Parse(playersOnTeam1.text));
+        PlayerPrefs.SetInt("Players Team2", int.Parse(playersOnTeam2.text));
 
-        PlayerPrefs.SetInt("Use Dynamic Difficulty Team1", useDynamicDifficultyTeam1 ? 1 : 0);
-        PlayerPrefs.SetInt("Use Dynamic Difficulty Team2", useDynamicDifficultyTeam2 ? 1 : 0);
+        PlayerPrefs.SetInt("Use Dynamic Difficulty Team1", useDynamicDifficultyTeam1.isOn ? 1 : 0);
+        PlayerPrefs.SetInt("Use Dynamic Difficulty Team2", useDynamicDifficultyTeam2.isOn ? 1 : 0);
 
-        PlayerPrefs.SetFloat("Difficulty Team1", difficultyTeam1);
-        PlayerPrefs.SetFloat("Difficulty Team2", difficultyTeam2);
+        PlayerPrefs.SetFloat("Difficulty Team1", float.Parse(difficultyTeam1.text));
+        PlayerPrefs.SetFloat("Difficulty Team2", float.Parse(difficultyTeam2.text));
 
         SceneManager.LoadScene(mapSelectionMenu.selectedMap);
     }
